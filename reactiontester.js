67363eeb627250234bb5413dffa0fbcfe998728e
Camilla Cofie <camillacofie@live.co.uk>
2018-01-08 22:02:10 +0000

    Add files via upload
    Created a function which generates a random colour every time the shape loads. Created another function which changes the shape and the size of the div every time it loads. Added some additional HTML to the page

--- reactiontester.js
+++ reactiontester.js
@@ -1,9 +1,44 @@
             
             var start = new Date().getTime();
+
+            function getRandomColour() {
+                
+                var letters = "0123456789ABCDEF".split('');
+                
+                var colour = "#";
+                
+                for (var i=0; i < 6; i ++) {
+                    
+                    colour += letters[Math.floor(Math.random() * 16)];
+                }
+                
+                return colour;
+            }
             
             function makeShapeAppear() {
                 
-              document.getElementById("shape").style.display = "block";
+                var top = Math.random() * 400;
+                
+                var left = Math.random() * 400;
+                
+                var width = (Math.random() * 300) + 100;
+                
+                if (Math.random() > 0.5) {
+                     document.getElementById("shape").style.borderRadius = "50%";
+                } else {
+                    
+                    document.getElementById("shape").style.borderRadius = "0";
+                }
+                
+            document.getElementById("shape").style.backgroundColor = getRandomColour();
+                
+                document.getElementById("shape").style.width = width + "px";
+                
+                document.getElementById("shape").style.height = width + "px";
+                document.getElementById("shape").style.top = top + "px";
+                
+                document.getElementById("shape").style.left = left + "px";
+                document.getElementById("shape").style.display = "block";
                 
                 start = new Date().getTime();
                 
